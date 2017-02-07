521baeaa094462e6ebc839958dca245c72fea9b8
Johan Martin Johannessen <johannes@sd.no>
2017-02-07 05:50:22 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+
+hbs.registerPartials(__dirname + "/views/partials")
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, resp, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log' , log + "\n", (err) => {
+        if(err){
+            console.log("Problemer med å skrive til logg");
+        }
+    });
+        next();
+    
+});
+
+app.use((req, res, next) => {
+     res.render('maintenance.hbs', {
+     pageTitle: "maintenance",
+     maintenanceMessage: "This site is under maintenance"
+     });
+ });
+
+app.get('/', (req, res) => {
+    
+hbs.registerHelper('getCurrentYear',() => {
+    return new Date().getFullYear();
+});
+    res.render('welcome.hbs',{
+        pageTitle: "Velkommen",
+        velkomsttekst: "Velkommen"
+    });
+});
+
+app.get("/about", (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
+    
+});
+
+app.get("/bad", (req, res) => {
+    res.send({
+        errorMessage: "Noe gikk galt"
+    });
+});
+
+
+ 
+
+app.listen(8081, () => {
+    console.log("Serveren startet på port 8081");
+}); 
